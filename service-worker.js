e90eec39c852accc76335fff02adf057c1309c76
Pavel Klochkov <pklochkov@degiro.nl>
2018-12-19 09:48:38 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,23 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts("/CurrencyExchangeApplication/precache-manifest.63659c03bcb3eb37724adf7e9917367e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+workbox.skipWaiting()
+workbox.clientsClaim()
 
-importScripts(
-  "/CurrencyExchangeApplication/precache-manifest.63659c03bcb3eb37724adf7e9917367e.js"
+workbox.routing.registerRoute(
+    // 'http://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+    new RegExp('(https):.*min\.(css|js)'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'cdn-cache'
+    })
 );
 
-workbox.clientsClaim();
+workbox.routing.registerRoute(
+    new RegExp('(https):\\/\\/openexchangerates.org\\/api\\/(latest.json).*'),
+    workbox.strategies.networkFirst({
+        cacheName: 'api-data'
+    })
+);
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+workbox.core.setLogLevel(workbox.core.LOG_LEVELS.silent);
 
-workbox.routing.registerNavigationRoute("/CurrencyExchangeApplication/index.html", {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
-});
