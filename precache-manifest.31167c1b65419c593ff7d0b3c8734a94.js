0c4850e2e70178fc6e347e61f75ae1d36ca3e5e4
Pavel Klochkov <paul.klochkov@gmail.com>
2019-05-25 17:21:53 +0200

    Updates

---
+++ precache-manifest.31167c1b65419c593ff7d0b3c8734a94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40d924c9b80927f5d851d170cbe5220",
+    "url": "/CurrencyExchangeApplication/index.html"
+  },
+  {
+    "revision": "3a48360db6c90cb27485",
+    "url": "/CurrencyExchangeApplication/static/css/main.5e8383ab.chunk.css"
+  },
+  {
+    "revision": "989ac650284373602ae3",
+    "url": "/CurrencyExchangeApplication/static/js/2.52da17a9.chunk.js"
+  },
+  {
+    "revision": "3a48360db6c90cb27485",
+    "url": "/CurrencyExchangeApplication/static/js/main.e18cc466.chunk.js"
+  },
+  {
+    "revision": "f16c3a74582e87992785",
+    "url": "/CurrencyExchangeApplication/static/js/runtime~main.2aa357b7.js"
+  }
+]);
